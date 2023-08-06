ff78b8d204d641d74a3b02c4bf996e4588e7a11d
doom <140380232+dooms-scripts@users.noreply.github.com>
2023-08-06 15:47:09 -0700

    Create stats-hook.lua

---
+++ stats-hook.lua
@@ -0,0 +1,63 @@
+--> Variables <-------------------------------------------------------------
+local https = game:GetService("HttpService")
+
+local WebhookURL = _G.Webhook -- The Discord Webhook URL it'll use to send the embed through.
+local userThumbnail = loadstring(game:HttpGet('https://pastebin.com/raw/AC35HBac'))()
+----------------------------------------------------------------------------
+
+warn('yeahyeah2')
+
+--> Functions <-------------------------------------------------------------
+coroutine.wrap(function()
+
+    -- Wait for player to load before running
+	game.Players.LocalPlayer:WaitForChild('DataFolder')
+
+    -- Loops until the player is kicked
+    local function sendWebhook()
+        local Player = game.Players.LocalPlayer
+        local Currency = Player.DataFolder.Currency.Value
+    
+        local formattedMoney = string.format("%d", Currency)
+    
+        local function formatNumberWithCommas(amount)
+            local left, num, right = string.match(tostring(amount), "^([^%d]*%d)(%d*)(.-)$")
+            return left .. (num:reverse():gsub("(%d%d%d)", "%1,"):reverse()) .. right
+        end
+    
+        formattedMoney = formatNumberWithCommas(formattedMoney)
+    
+        local embColor = 0x1CA6E4
+    
+        local data = {
+            Url = WebhookURL,
+            Method = "POST",
+            Headers = {
+                ["Content-Type"] = "application/json"
+            },
+            Body = game:GetService("HttpService"):JSONEncode({
+                embeds = {{
+                    author = {
+                        name = "dooms autofarm",
+                        icon_url = "https://cdn.discordapp.com/attachments/1136792419057283082/1137763658332651520/blueinfo.png"
+                    },
+                    title = string.format("%s (%s) - Stats.", Player.Name, Player.UserId),
+                    description = string.format("Money: `%s` \nElapsed: `%s seconds` \nATMs Farmed: `%s`", formattedMoney, tostring(_G.Elapsed), tostring(_G.ATMCount)),
+                    thumbnail = {
+                        url = tostring(userThumbnail),
+                    },
+                    color = embColor
+                }}
+            })
+        }
+            
+        local response = http.request(data)
+    end
+    
+    -- Sends the webhook every 10 minutes
+    while true do
+        sendWebhook()
+        wait(600)
+    end
+end)()
+----------------------------------------------------------------------------
