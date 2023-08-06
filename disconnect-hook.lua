7794672806c3b3568919c66274588438a072ceb1
doom <140380232+dooms-scripts@users.noreply.github.com>
2023-08-06 06:31:37 -0700

    Create disconnect-hook.lua

---
+++ disconnect-hook.lua
@@ -0,0 +1,73 @@
+--> Variables <-------------------------------------------------------------
+local https = game:GetService("HttpService")
+
+local WebhookURL = _G.Webhook -- The Discord Webhook URL it'll use to send the embed through.
+local userThumbnail = loadstring(game:HttpGet('https://pastebin.com/raw/AC35HBac'))()
+
+local debounce = false
+_G.errMsg = 'Couldnt fetch'
+----------------------------------------------------------------------------
+
+--> Functions <-------------------------------------------------------------
+coroutine.wrap(function()
+
+  -- Wait for player to load before running
+	game.Players.LocalPlayer:WaitForChild('DataFolder')
+
+  -- Loops until the player is kicked
+	while debounce == false do wait()
+		local function sendWebhook()
+		    local Player = game.Players.LocalPlayer
+		    local Currency = Player.DataFolder.Currency.Value
+		
+		    local formattedMoney = string.format("%d", Currency)
+		
+		    local function formatNumberWithCommas(amount)
+		        local left, num, right = string.match(tostring(amount), "^([^%d]*%d)(%d*)(.-)$")
+		        return left .. (num:reverse():gsub("(%d%d%d)", "%1,"):reverse()) .. right
+		    end
+		
+		    formattedMoney = formatNumberWithCommas(formattedMoney)
+		
+		    local embColor = 0xEF2D24
+		
+		    local data = {
+		        Url = WebhookURL,
+		        Method = "POST",
+		        Headers = {
+		            ["Content-Type"] = "application/json"
+		        },
+		        Body = game:GetService("HttpService"):JSONEncode({
+		            embeds = {{
+		                author = {
+		                    name = "dooms autofarm",
+		                    icon_url = "https://cdn.discordapp.com/attachments/1136792419057283082/1137717097879834685/info.png"
+		                },
+		                title = string.format("%s (%s) Disconnected.", Player.Name, Player.UserId),
+		                description = string.format("Money: `%s` \nReason for kick: `%s` \nElapsed:", formattedMoney, tostring(_G.errMsg)),
+		                thumbnail = {
+		                    url = tostring(userThumbnail),
+		                },
+		                color = embColor
+		            }}
+		        })
+			}
+			   
+		    local response = http.request(data)
+		end
+		
+		-- Check for kick message
+		for _,v in ipairs(game.CoreGui.RobloxPromptGui.promptOverlay:GetDescendants()) do
+
+      -- If an error mesage is found, sets the errMsg and sends the webhook.
+			if v.Name == 'ErrorMessage' then
+				_G.errMsg = v.Text:gsub('You were kicked from this experience: ','')
+				_G.errMsg = _G.errMsg:gsub('\n', ' ')
+
+				sendWebhook()
+				debounce=true
+			end
+		end
+	end
+end)()
+----------------------------------------------------------------------------
